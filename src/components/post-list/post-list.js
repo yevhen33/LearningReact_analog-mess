3e155dc06ac996fec0f5b5d5a0532c4269f66745
Yevhen <yevhen.code@gmail.com>
2020-06-21 21:38:34 +0300

    added verification of data from the server to void and not an object

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -7,13 +7,23 @@ import PostListItem from '../post-list-item';
 const PostList = ({posts}) => {
 
     const elements = posts.map((item) => {
-        const {id, ...itemProps} = item;
-        return (
-            <li key={id} className='list-group-item'>
-                <PostListItem {...itemProps}/> 
-            </li>
-        )
-    });
+
+        if (typeof item === 'object' && isEmpty(item)) {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps}/> 
+                </li>
+            )
+        }
+    })
+
+    function isEmpty(obj) {
+        for (let key in obj) {
+            return true;
+        }
+        return false;
+    }
 
     return (
         <ul className="app-list list-group">
